c0f4cf24c7073eda149ca58319f0727c72a0c905
Minahil <mimahilirshad786@gmail.com>
2022-09-18 22:56:46 +0500

    testing

---
+++ app/src/main/java/com/example/i192178/playlist2.java
@@ -0,0 +1,14 @@
+package com.example.i192178;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class playlist2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_playlist2);
+    }
+}
---
+++ app/src/main/java/com/example/i192178/record3.java
@@ -0,0 +1,14 @@
+package com.example.i192178;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class record3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_record3);
+    }
+}
